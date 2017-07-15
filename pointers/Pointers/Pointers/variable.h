8c53978d50c55abd42c76e67638c3c5d27a9a061
Zenkin <zenkinartem1997@gmail.com>
2017-07-15 17:25:21 +0300

    commited

---
+++ pointers/Pointers/Pointers/variable.h
@@ -0,0 +1,15 @@
+#ifndef VARIABLE_H
+#define VARIABLE_H
+
+#include<iostream>
+
+struct Variable {
+    int int_size;
+    float float_size;
+    double double_size;
+    int *int_adress;
+    float *float_adress;
+    double *douvle_adress;
+} variable;
+
+#endif // VARIABLE_H
