7561f9b34a4d073317a9cd6c59789e41ea94f02d
Zenkin <zenkinartem1997@gmail.com>
2017-07-16 16:52:17 +0300

    commited

--- pointers/Pointers/Pointers/assigning_values_using_pointers.cpp
+++ pointers/Pointers/Pointers/assigning_values_using_pointers.cpp
@@ -6,13 +6,36 @@ void assigning_values_using_pointers() {
 
    int some_kind_of_variable;
    some_kind_of_variable = 100;
-   std::cout << "It is value of some kind of variable: " << some_kind_of_variable << std::endl;
+   std::cout << "It is value of some kind of int variable: " << some_kind_of_variable << std::endl;
 
    int *pointer;
    pointer = &some_kind_of_variable;
-   std::cout << "It is adress of some kind of variable: " << pointer << std::endl
+   std::cout << "It is adress of some kind of int variable: " << pointer << std::endl
    << std::endl;
 
    (*pointer)++;
-   std::cout << "We implicitly increase the value of our variable: " << some_kind_of_variable << std::endl;
+   std::cout << "We implicitly increase the value of our int variable: " << some_kind_of_variable << std::endl;
+
+   pointer ++;
+   std::cout << "We use pointer++ : " << *pointer << "  | adress: " << pointer << std::endl;
+   pointer --;
+   std::cout << "We use pointer-- : " << *pointer << "  | adress: " << pointer << std::endl;
+   pointer --;
+   std::cout << "We use pointer-- : " << *pointer << "  | adress: " << pointer << std::endl;
+   pointer = pointer + 1;
+   std::cout << "We use pointer + 1 : " << *pointer << "  | adress: " << pointer << std::endl;
+   pointer = pointer + 1;
+   std::cout << "We use pointer + 1 : " << *pointer << "  | adress: " << pointer << std::endl;
+   pointer = pointer - 2;
+   std::cout << "We use pointer - 2 : " << *pointer << "  | adress: " << pointer << std::endl
+   << std::endl;
+
+   int some_kind_of_variable_2;
+   std::cout << "It is value of the second some kind of int variable: " << some_kind_of_variable_2 << std::endl;
+
+   int *pointer_2;
+   pointer_2 = &some_kind_of_variable_2;
+   std::cout << "It is adress of the second some kind of int variable: " << pointer_2 << std::endl;
+
+   std::cout << "We use pointer - pointer_2: " << pointer - pointer_2 << " number of elements of the base type that share these two pointers" << std::endl;
 }
