9a08f01bbc28acfcacd050278f14945e63a2b795
Zenkin <zenkinartem1997@gmail.com>
2017-07-25 14:08:15 +0300

    commited

--- class/class/main.cpp
+++ class/class/main.cpp
@@ -1,5 +1,23 @@
 #include"queue.h"
 
 int main(int argc, char *argv[]) {
-    queue Q1, Q2;
+    queue q1, q2;
+    q1.init();
+    q2.init();
+
+    q1.qput(10);
+    q2.qput(19);
+
+    q1.qput(20);
+    q2.qput(1);
+
+    std::cout << "q1 queue content: ";
+    std::cout << q1.qget() << " ";
+    std::cout << q1.qget() << "\n";
+
+    std::cout << "q2 queue content: ";
+    std::cout << q2.qget() << " ";
+    std::cout << q2.qget() << "\n";
+
+    return 0;
 }
--- class/class/queue.h
+++ class/class/queue.h
@@ -1,4 +1,37 @@
 #ifndef CLASS_H
 #define CLASS_H
 
+#include<iostream>
+
+class queue {
+        int q[100];
+        int sloc, rloc;
+    public:
+        int qget();
+        void init();
+        void qput(int i);
+};
+
+void queue::qput(int i) {
+    if(sloc==100) {
+        std::cout << "The queue is full. \n";
+        return;
+    }
+    sloc++;
+    q[sloc] =  i;
+}
+
+void queue::init() {
+    rloc = sloc = 0;
+}
+
+int queue::qget() {
+    if(rloc == sloc) {
+        std::cout << "The queue is empty. \n";
+        return 0;
+    }
+    rloc++;
+    return q[rloc];
+}
+
 #endif // CLASS_H
