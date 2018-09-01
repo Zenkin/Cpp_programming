0962df4366abd8529829f34ffe9397096ba6f4da
Zenkin <zenkinartem1997@gmail.com>
2018-09-01 13:15:23 +0300

    myStrcat

--- functions_basic_lesson/main.cpp
+++ functions_basic_lesson/main.cpp
@@ -4,22 +4,36 @@ using namespace std;
 
 void function(int *parameter);
 void cube(int *p_array, int length);
+int getStrLength(char *str);
+void myStrcat(char *where2Copy, const char *what2Copy);
 
 int main() {
-    int parameter = 100;
-    int division_by_10 = 2;
-    for(int index = 0; index < division_by_10; index++) function(&parameter);
-
-    int size = 10;
-    int array[size];
-    for(int i = 0; i < size; i++) array[i] = i+1;
-    for(int i = 0; i < size; i++) cout << array[i] << ' ';
-    cout << endl;
-    cube(array, size);
-    for(int i = 0; i < size; i++) cout << array[i] << ' ';
-    cout << endl;
+    char str1[100] = "Hello";
+    char str2[] = " World!";
+    myStrcat(str1, str2);
+    cout << str1 << endl;
 
     return 0;
+}
+
+void myStrcat(char *where2Copy, const char *what2Copy) {
+    int where2CopyLength;
+    for(where2CopyLength = 0; where2Copy[where2CopyLength]; where2CopyLength++);
+    int what2CopyLength;
+    for(what2CopyLength = 0; what2Copy[what2CopyLength]; what2CopyLength++);
+    for(int index = where2CopyLength; index < where2CopyLength+what2CopyLength; index++) {
+        where2Copy[index] = what2Copy[index-where2CopyLength];
+    }
+    where2Copy[where2CopyLength+what2CopyLength] = '\0';
+}
+
+int getStrLength(char *str) {
+    int i = 0;
+    while (*(str+i)) {
+        i++;
+    }
+
+    return i;
 }
 
 void function(int *parameter) {
