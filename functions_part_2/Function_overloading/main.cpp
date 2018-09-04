e6ba97e353c15bba63b255e377efa0522f99b95f
Zenkin <zenkinartem1997@gmail.com>
2018-09-04 19:15:06 +0300

    myabs

--- functions_part_2/Function_overloading/main.cpp
+++ functions_part_2/Function_overloading/main.cpp
@@ -2,7 +2,29 @@
 
 using namespace std;
 
-int main() {
-    cout << "Hello World!" << endl;
+int myabs(int i);
+double myabs(double i);
+long myabs(long i);
+
+int main() { 
+    cout << myabs(-5) << endl;
+    cout << myabs(-1.1) << endl;
+    cout << myabs(-9L) << endl;
+
     return 0;
+}
+
+int myabs(int i) {
+   if(i < 0) return -i;
+   else return i;
+}
+
+double myabs(double i) {
+   if(i < 0) return -i;
+   else return i;
+}
+
+long myabs(long i) {
+   if(i < 0) return -i;
+   else return i;
 }
