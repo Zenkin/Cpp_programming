6766b20e9eef8dcfb1cff3136e1207865ff082aa
Zenkin <zenkinartem1997@gmail.com>
2018-09-04 19:47:44 +0300

    mystrcat

--- functions_part_2/Function_overloading/main.cpp
+++ functions_part_2/Function_overloading/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
@@ -6,15 +7,51 @@ int myabs(int i);
 double myabs(double i);
 long myabs(long i);
 void clrscr(int size = 30);
+void mystrcat(char *str1, char *str2, int len = -1);
+int mylen(char *str);
 
 int main() { 
-    cout << myabs(-5) << endl;
-    cout << myabs(-1.1) << endl;
-    cout << myabs(-9L) << endl;
+    cout << "Test myabs: \n ";
+    cout << myabs(-5) << "\n ";
+    cout << myabs(-1.1) << "\n ";
+    cout << myabs(-9L) << "\n\n";
 
-    clrscr();
+    cout << "Test mystrcat: \n ";
+
+    char str1[80] = "Hello";
+    char str2[80] = " World";
+
+    cout << str1 << "\n ";
+    mystrcat(str1, str2);
+    cout << str1 << "\n";
+    cout << ">> Refresh str1\n ";
+
+    strcpy(str1, "Hello");
+
+    cout << str1 << "\n ";
+    mystrcat(str1, str2, 5);
+    cout << str1 << "\n\n";
 
     return 0;
+}
+
+void mystrcat(char *str1, char *str2, int len) {
+    while (*str1) str1++;
+    if(len == -1) len = mylen(str2);
+    while(*str2 && len) {
+        *str1 = *str2;
+        str1++; str2++; len--;
+    }
+    *str1 = '\0';
+}
+
+int mylen(char *str) {
+    int size = 0;
+    while(*str) {
+        size++;
+        str++;
+    }
+    return size;
 }
 
 void clrscr(int size) {
