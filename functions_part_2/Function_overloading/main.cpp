c913e5f5f2ea44049b0d4bf9d17be1b37d23c092
Zenkin <zenkinartem1997@gmail.com>
2018-09-04 19:26:30 +0300

    clrscr

--- functions_part_2/Function_overloading/main.cpp
+++ functions_part_2/Function_overloading/main.cpp
@@ -5,13 +5,20 @@ using namespace std;
 int myabs(int i);
 double myabs(double i);
 long myabs(long i);
+void clrscr(int size = 30);
 
 int main() { 
     cout << myabs(-5) << endl;
     cout << myabs(-1.1) << endl;
     cout << myabs(-9L) << endl;
 
+    clrscr();
+
     return 0;
+}
+
+void clrscr(int size) {
+    for(; size; size--) cout << '\n';
 }
 
 int myabs(int i) {
