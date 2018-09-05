8e0b74175d82827711861f7019affea40210d7b3
Zenkin <zenkinartem1997@gmail.com>
2018-09-05 18:48:11 +0300

    avg

--- data_types_and_operators_part_2/main.cpp
+++ data_types_and_operators_part_2/main.cpp
@@ -2,11 +2,27 @@
 
 using namespace std;
 
+double avg(double i);
+
 int main() {
     extern int it, is, magic;
     cout << it << ' ' << is << ' ' <<  magic << '\n';
 
+    double num;
+    do {
+        cout << "Enter number (-1 out): ";
+        cin >> num;
+        if (num != -1) cout << "avg = " << avg(num) << '\n';
+    } while (num != -1);
+
     return 0;
+}
+
+double avg(double i) {
+    static double sum = 0, count = 0;
+    sum += i;
+    count++;
+    return sum/count;
 }
 
 int it = 10, is = 20, magic = 30;
