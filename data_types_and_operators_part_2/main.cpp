a829e7242c919f10867806c906027fdefb645097
Zenkin <zenkinartem1997@gmail.com>
2018-09-06 21:30:25 +0300

    signed_pwr

--- data_types_and_operators_part_2/main.cpp
+++ data_types_and_operators_part_2/main.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 double avg(double i);
+int signed_pwr(register int m, register int e);
 
 int main() {
     extern int it, is, magic;
@@ -15,7 +16,19 @@ int main() {
         if (num != -1) cout << "avg = " << avg(num) << '\n';
     } while (num != -1);
 
+    cout << signed_pwr(-4, 2) << '\n';
+
     return 0;
+}
+
+int signed_pwr(int m, int e) {
+    register int temp;
+    int sign;
+    if(m < 0) sign = -1;
+    else sign = 1;
+    temp = 1;
+    for(; e; e--) temp = temp * m;
+    return temp * sign;
 }
 
 double avg(double i) {
