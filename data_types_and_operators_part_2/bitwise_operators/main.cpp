fb6f67a4cae9f7af0c56491065a1f922d481a0da
Zenkin <zenkinartem1997@gmail.com>
2018-09-09 21:00:15 +0300

    toUp

--- data_types_and_operators_part_2/bitwise_operators/main.cpp
+++ data_types_and_operators_part_2/bitwise_operators/main.cpp
@@ -2,8 +2,18 @@
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+void toUp(char &ch);
+
+int main() {
+    char ch;
+    cin >> ch;
+    cout << "ch = " << ch;
+    toUp(ch);
+    cout << " | ch & 95 = " << ch << '\n';
+
     return 0;
+}
+
+void toUp(char &ch) {
+    ch = ch & 95;
 }
