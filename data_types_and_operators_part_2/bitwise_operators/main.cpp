5c9582cb8f0ac68846e3c60e55950302f60f8b18
Zenkin <zenkinartem1997@gmail.com>
2018-09-10 12:44:48 +0300

    >> AND <<

--- data_types_and_operators_part_2/bitwise_operators/main.cpp
+++ data_types_and_operators_part_2/bitwise_operators/main.cpp
@@ -7,9 +7,19 @@ void toDown(char &ch);
 void disp_binary(unsigned u);
 
 int main() {
-    unsigned u;
-    cout << "Enter 0-255: "; cin >> u;
-    disp_binary(u);
+    int i = 1, j = 128, t;
+    for(t = 0; t < 8; t++) {
+        disp_binary(i); cout << "  "; disp_binary(j); cout << '\n';
+        i = i << 1;
+        j = j >> 1;
+    }
+    cout << '\n';
+    j = 1; i = 128;
+    for(t = 0; t < 8; t++) {
+        disp_binary(i); cout << "  "; disp_binary(j); cout << '\n';
+        i = i >> 1;
+        j = j << 1;
+    }
 
     return 0;
 }
@@ -20,7 +30,7 @@ void disp_binary(unsigned u) {
         if(u & t) cout << '1';
         else cout << '0';
     }
-    cout << '\n';
+    //cout << '\n';
 }
 
 void toUp(char &ch) {
