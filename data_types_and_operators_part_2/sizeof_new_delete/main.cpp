8d62333dc0fad542004abb7d13d06977e398ac7c
Zenkin <zenkinartem1997@gmail.com>
2018-09-10 13:34:00 +0300

    new/delete

--- data_types_and_operators_part_2/sizeof_new_delete/main.cpp
+++ data_types_and_operators_part_2/sizeof_new_delete/main.cpp
@@ -2,8 +2,21 @@
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+int main() {
+    char ch;
+    int i;
+    cout << sizeof ch << " byte\n";
+    cout << sizeof i << " byte\n";
+    cout << sizeof (float) << " byte\n";
+    cout << sizeof (double) << " byte\n";
+    int num[4];
+    cout << sizeof num << " byte\n";
+
+    int *p;
+    p = new int;
+    *p = 20;
+    cout << *p << '\n';
+    delete p;
+
     return 0;
 }
