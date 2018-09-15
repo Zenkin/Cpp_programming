6423339394828d97d1127dba9d181744def3328f
Zenkin <zenkinartem1997@gmail.com>
2018-09-15 15:37:19 +0300

    getTime

--- struct_lessons/main.cpp
+++ struct_lessons/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
@@ -6,11 +7,23 @@ struct my_type {
     int a, b;
 } type_val;
 
+void getTime();
+
 int main() {
     my_type *p;
     p = new my_type;
     p->a = 1;
-    cout << p->a << '\n';
+
+    while(1) getTime();
 
     return 0;
+}
+
+void getTime() {
+    struct tm *ptr;
+    time_t curtime;
+    time(&curtime);
+    ptr = localtime(&curtime);
+    cout << ptr->tm_hour << ':' << ptr->tm_min;
+    cout << ':' <<	ptr->tm_sec << '\n';
 }
