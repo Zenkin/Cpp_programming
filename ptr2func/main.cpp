4a44cfe1f010f2610f0f2767f205cf1aa51d9c2c
Artemii <zenkinartem1997@gmail.com>
2019-08-24 22:49:47 +0300

    ptr2func

---
+++ ptr2func/main.cpp
@@ -0,0 +1,25 @@
+// Наибольший общий делитель НОД
+
+#include <iostream>
+
+using namespace std;
+
+int nod(int, int);
+
+int main(int argc, char* argv[]) {
+    int (*ptr2nod)(int, int);
+    ptr2nod = nod;
+    int a, b;
+    cout << "Enter first number: ";
+    cin >> a;
+    cout << "Enter second number: ";
+    cin >> b;
+    cout << "NOD = " << ptr2nod(a, b) << endl;
+
+    return 0;
+}
+
+int nod(int number1, int number2) {
+    if (number2 == 0) return number1;
+    return nod(number2, number1 % number2);
+}
