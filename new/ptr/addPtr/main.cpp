c220c66a03ccc86f32b8624a2a45a574def65543
Artemii <zenkinartem1997@gmail.com>
2019-09-01 00:54:14 +0300

    delele_str

--- new/ptr/addPtr/main.cpp
+++ new/ptr/addPtr/main.cpp
@@ -5,13 +5,12 @@ using namespace std;
 
 char *add_char(char *p_char, int *size, char ch);
 char **add_str(char **p_char2, int *size, char *str);
-char **free_memory_pp(char **pp_p_char, int size);
+char **delete_str(char **pp_char, int *size, int del_2pose);
 
 int main() {
 
     char *p_char = 0;
     int size = 0;
-
     p_char = add_char(p_char, &size, 'I');
     p_char = add_char(p_char, &size, ' ');
     p_char = add_char(p_char, &size, 'A');
@@ -29,34 +28,52 @@ int main() {
     p_char = add_char(p_char, &size, 'T');
     p_char = add_char(p_char, &size, 'S');
     p_char = add_char(p_char, &size, '!');
-
     for(int i = 0; i < size; i++) {
         cout << p_char[i];
     }
     cout << endl;
-
     delete[] p_char;
 
     char **pp_char = 0;
     size = 0;
-    pp_char = add_str(pp_char, &size, "I AM ");
-    pp_char = add_str(pp_char, &size, "LOVE ROBOTS!");
-
+    pp_char = add_str(pp_char, &size, "111");
+    pp_char = add_str(pp_char, &size, "222");
+    pp_char = add_str(pp_char, &size, "333");
     for(int i = 0; i < size; i++) {
-        cout << pp_char[i];
+        cout << pp_char[i] << endl;
     }
     cout << endl;
-
-    pp_char = free_memory_pp(pp_char, size);
+    pp_char = delete_str(pp_char, &size, 1);
+    for(int i = 0; i < size; i++) {
+        cout << pp_char[i] << endl;
+    }
 
     return 0;
 }
 
-char **free_memory_pp(char **pp_p_char, int size) {
-    for(int i = 0; i < size; i++) {
-        delete[] pp_p_char[i];
-    }
-    delete[] pp_p_char;
+char **delete_str(char **pp_char, int *size, int del_pose) {
+        char **pp_char_copy = new char *[*size];
+        for(int i = 0; i < (*size); i++) {
+            pp_char_copy[i] = pp_char[i];
+        }
+        for(int i = del_pose; i < (*size) - 1; i++) {
+            pp_char_copy[i] = pp_char_copy[i+1];
+        }
+        for(int i = 0; i < (*size); i++) {
+            delete[] pp_char[i];
+        }
+        delete[] pp_char;
+        pp_char = new char *[(*size) - 1];
+        for(int i = 0; i < (*size) - 1; i++) {
+            pp_char[i] = pp_char_copy[i];
+        }
+        for(int i = 0; i < (*size); i++) {
+            delete[] pp_char_copy[i];
+        }
+        delete[] pp_char_copy;
+        (*size)--;
+
+        return pp_char;
 }
 
 char **add_str(char **pp_char, int *size, char *str) {
@@ -69,13 +86,19 @@ char **add_str(char **pp_char, int *size, char *str) {
         for(int i = 0; i < *size; i++) {
             pp_char_copy[i] = pp_char[i];
         }
-        pp_char = free_memory_pp(pp_char, *size);
+        for(int i = 0; i < (*size); i++) {
+            delete[] pp_char[i];
+        }
+        delete[] pp_char;
         pp_char = new char *[*size+1];
         for(int i = 0; i < *size; i++) {
             pp_char[i] = pp_char_copy[i];
         }
         pp_char[*size] = str;
-        pp_char_copy = free_memory_pp(pp_char_copy, *size);
+        for(int i = 0; i < (*size); i++) {
+            delete[] pp_char_copy[i];
+        }
+        delete[] pp_char_copy;
     }
     (*size)++;
     return pp_char;
